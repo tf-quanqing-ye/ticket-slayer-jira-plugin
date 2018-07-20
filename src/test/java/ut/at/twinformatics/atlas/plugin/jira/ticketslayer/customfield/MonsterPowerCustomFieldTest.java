0edc51231e71b7d27df3592a5ad3eaa1e623be89
Qjay <Quanqing.ye@twinformatics.at>
2018-07-20 15:38:29 +0200

    tutorial custom field

---
+++ src/test/java/ut/at/twinformatics/atlas/plugin/jira/ticketslayer/customfield/MonsterPowerCustomFieldTest.java
@@ -0,0 +1,34 @@
+package ut.at.twinformatics.atlas.plugin.jira.ticketslayer.customfield;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import at.twinformatics.atlas.plugin.jira.ticketslayer.customfield.MonsterPowerCustomField;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class MonsterPowerCustomFieldTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //MonsterPowerCustomField testClass = new MonsterPowerCustomField();
+
+        throw new Exception("MonsterPowerCustomField has no tests!");
+
+    }
+
+}
