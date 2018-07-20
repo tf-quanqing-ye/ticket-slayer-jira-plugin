93654473e5d8532ba9b42f77665062a1df2b198a
Hao Tri Pham <HaoTri.Pham@twinformatics.at>
2018-07-20 16:20:27 +0100

    Change field to CalculatedCFType

--- src/main/java/at/twinformatics/atlas/plugin/jira/ticketslayer/customfield/MonsterPowerCustomField.java
+++ src/main/java/at/twinformatics/atlas/plugin/jira/ticketslayer/customfield/MonsterPowerCustomField.java
@@ -1,29 +1,43 @@
 package at.twinformatics.atlas.plugin.jira.ticketslayer.customfield;
 
-import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
-import com.atlassian.plugin.spring.scanner.annotation.imports.JiraImport;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import com.atlassian.jira.issue.customfields.impl.TextCFType;
-import com.atlassian.jira.issue.customfields.manager.GenericConfigManager;
-import com.atlassian.jira.issue.customfields.persistence.CustomFieldValuePersister;
-import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
 import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.customfields.impl.CalculatedCFType;
+import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
 import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.fields.config.FieldConfig;
 import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem;
-import java.util.List;
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Map;
 
 @Scanned
-public class MonsterPowerCustomField extends TextCFType {
+public class MonsterPowerCustomField extends CalculatedCFType<Integer, Integer> {
     private static final Logger log = LoggerFactory.getLogger(MonsterPowerCustomField.class);
 
-    public MonsterPowerCustomField(@JiraImport CustomFieldValuePersister customFieldValuePersister,
-                                   @JiraImport GenericConfigManager genericConfigManager) {
-    super(customFieldValuePersister, genericConfigManager);
-}
-    
+    public MonsterPowerCustomField() {
+    }
+
+    @Override
+    public String getStringFromSingularObject(final Integer integer) {
+        return integer != null ? integer.toString() : null;
+    }
+
+    @Override
+    public Integer getSingularObjectFromString(final String s) throws FieldValidationException {
+        try {
+            return s != null && !s.isEmpty() ? Integer.parseInt(s) : null;
+        } catch (final NumberFormatException e) {
+            throw new FieldValidationException(e.getMessage());
+        }
+    }
+
+    @Override
+    public Integer getValueFromIssue(CustomField customField, Issue issue) {
+        return 0;
+    }
+
     @Override
     public Map<String, Object> getVelocityParameters(final Issue issue,
                                                      final CustomField field,
